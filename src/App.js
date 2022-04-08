492dd8a9749e125bcb79e940c7f141aabbf8a116
abheetkumar1999 <abheetkumar1999@gmail.com>
2022-04-08 16:35:12 +0530

    Create a React app and publish it to GitHub Pages

--- src/App.js
+++ src/App.js
@@ -1,23 +1,180 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from "react";
+import {
+  numbers,
+  upperCaseLetters,
+  lowerCaseLetters,
+  specialCharacters,
+} from "./Character";
+import { toast, ToastContainer } from "react-toastify";
+import "react-toastify/dist/ReactToastify.css";
+import { COPY_Fail, COPY_SUCCESS } from "./message";
+import "./App.css";
 
 function App() {
+  const [password, setPassword] = useState("");
+  const [passwordLength, setPasswordLength] = useState(26);
+  const [includeUpperCase, setIncludeUpperCase] = useState(false);
+  const [includeLowerCase, setIncludeLowerCase] = useState(false);
+  const [includeNumbers, setIncludeNumbers] = useState(false);
+  const [includeSymbols, setIncludeSymbols] = useState(false);
+
+  const handleGeneratePassword = () => {
+    if (
+      !includeUpperCase &&
+      !includeLowerCase &&
+      !includeNumbers &&
+      !includeSymbols
+    ) {
+      notify(
+        "To generate the password you must select at least one of the checkbox",
+        true
+      );
+    } else {
+      let characterList = "";
+      if (includeLowerCase) {
+        characterList += lowerCaseLetters;
+      }
+      if (includeUpperCase) {
+        characterList += upperCaseLetters;
+      }
+      if (includeNumbers) {
+        characterList += numbers;
+      }
+      if (includeSymbols) {
+        characterList += specialCharacters;
+      }
+      setPassword(createPassword(characterList));
+      notify("The password is generated successfully", false);
+    }
+  };
+  const createPassword = (characterList) => {
+    let password = "";
+    const characterListLength = characterList.length;
+    for (let i = 0; i < passwordLength; i++) {
+      const characterIndex = Math.round(Math.random() * characterListLength);
+      password = password + characterList.charAt(characterIndex);
+    }
+    return password;
+  };
+
+  const copyPassword = (password) => {
+    navigator.clipboard.writeText(password);
+  };
+
+  const notify = (message, hasError = false) => {
+    if (hasError) {
+      toast.error(message, {
+        position: "top-left",
+        autoClose: 2000,
+        hideProgressBar: false,
+        closeOnClick: true,
+        pauseOnHover: true,
+        draggable: true,
+        progress: undefined,
+      });
+    } else {
+      toast(message, {
+        theme: "dark",
+        position: "top-left",
+        autoClose: 2000,
+        hideProgressBar: false,
+        closeOnClick: true,
+        pauseOnHover: true,
+        draggable: true,
+        draggablePercent: 60,
+        progress: undefined,
+      });
+    }
+  };
+  const handleCopyPassword = (e) => {
+    if (password === "") {
+      notify(COPY_Fail, true);
+    } else {
+      copyPassword(password);
+      notify(COPY_SUCCESS);
+    }
+  };
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div className="container">
+        <div className="generator">
+          <h2 className="generator_header">Password Generator</h2>
+          <div className="generator_password">
+            <h3>{password}</h3>
+            <button className="copy_btn">
+              <i onClick={handleCopyPassword} className="far fa-clipboard"></i>
+            </button>
+          </div>
+          <div className="form-group">
+            <label htmlFor="password-strength">Password length</label>
+            <input
+              className="pw"
+              defaultValue={passwordLength}
+              onChange={(e) => setPasswordLength(e.target.value)}
+              type="number"
+              id="password-stregth"
+              name="password-strength"
+              max="26"
+              min="8"
+            />
+          </div>
+          <div className="form-group">
+            <label htmlFor="uppercase-letters">Add Uppercase Letters</label>
+            <input
+              checked={includeUpperCase}
+              onChange={(e) => setIncludeUpperCase(e.target.checked)}
+              type="checkbox"
+              id="uppercase-letters"
+              name="uppercase-letters"
+            />
+          </div>
+          <div className="form-group">
+            <label htmlFor="lowercase-letters">Add Lowercase Letters</label>
+            <input
+              checked={includeLowerCase}
+              onChange={(e) => setIncludeLowerCase(e.target.checked)}
+              type="checkbox"
+              id="lowercase-letters"
+              name="lowercase-letters"
+            />
+          </div>
+          <div className="form-group">
+            <label htmlFor="include-numbers">Include Numbers</label>
+            <input
+              checked={includeNumbers}
+              onChange={(e) => setIncludeNumbers(e.target.checked)}
+              type="checkbox"
+              id="include-numbers"
+              name="include-numbers"
+            />
+          </div>
+          <div className="form-group">
+            <label htmlFor="include-symbols">Include Symbols</label>
+            <input
+              checked={includeSymbols}
+              onChange={(e) => setIncludeSymbols(e.target.checked)}
+              type="checkbox"
+              id="include-symbols"
+              name="include-symbols"
+            />
+          </div>
+          <button onClick={handleGeneratePassword} className="generator_btn">
+            Generate Password
+          </button>
+          <ToastContainer
+            position="top-left"
+            autoClose={2000}
+            hideProgressBar={false}
+            newestOnTop={false}
+            closeOnClick
+            rtl={false}
+            pauseOnFocusLoss
+            draggable
+            pauseOnHover
+          />
+        </div>
+      </div>
     </div>
   );
 }
